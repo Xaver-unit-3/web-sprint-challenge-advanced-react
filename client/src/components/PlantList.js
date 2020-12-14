9593ce119efa98f8a9aada0404fa3ddf6a313125
Xaver Pinero <xaver.pinero@gmail.com>
2020-12-13 19:15:04 -0500

    added constructor and axios call for class components

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -7,6 +7,25 @@ export default class PlantList extends Component {
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  constructor() {
+    super();
+    this.state = {
+      plants: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/plants')
+      .then((res) => {
+        console.log(res)
+        this.setState({
+          plants: res.data.plantsData
+        });
+      })
+      .catch((err) => console.log('error', err));
+  }
+  
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
